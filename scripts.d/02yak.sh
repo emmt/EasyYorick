ee0362b6010e4f8bda646f704a29a7017eba132a
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2025-09-22 16:10:20 +0200

    Fix directory for Yak

--- scripts.d/02yak.sh
+++ scripts.d/02yak.sh
@@ -5,31 +5,31 @@
 ypkg_define "yak" "https://github.com/emmt/YakMessenger.jl.git"
 
 clone_yak() {
-    if ! test -d "$SRCDIR/YakMessenger.jl"
+    if ! test -d "$SRCDIR/yak"
     then
         cd "$SRCDIR"
-        git clone "$YGIT_ORIGIN" "YakMessenger.jl"
+        git clone "$YGIT_ORIGIN" "yak"
     fi
 }
 
 update_yak() {
     clone_yak
-    cd "$SRCDIR/YakMessenger.jl"
+    cd "$SRCDIR/yak"
     git pull -r
 }
 
 config_yak() {
-    test -d "$SRCDIR/YakMessenger.jl" || clone_yak
-    test -d "$SRCDIR/YakMessenger.jl/yorick" || update_yak
+    test -d "$SRCDIR/yak" || clone_yak
+    test -d "$SRCDIR/yak/yorick" || update_yak
 }
 
 build_yak() {
-    test -d "$SRCDIR/YakMessenger.jl/yorick" || config_yak
+    test -d "$SRCDIR/yak/yorick" || config_yak
 }
 
 install_yak() {
     local dir
-    dir="$SRCDIR/YakMessenger.jl/yorick"
+    dir="$SRCDIR/yak/yorick"
     test -d "$dir" || build_yak
     cp -a "$dir/yak.i" "$Y_SITE/i0/yak.i"
     cp -a "$dit/yak-start.i" "$Y_SITE/i-start/yak-start.i"
